3587ee59ba40ce98ca985f3f03605e725224ef03
jgoldfeder <jgoldfed@mail.yu.edu>
2022-04-06 23:07:25 -0400

    added filtering

--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@ from sqlalchemy import *
 
 class Backend:
     def __init__(self):
-        #self.engine = create_engine('postgresql://'+"postgres"+':'+'pass'+'@localhost:5432/db1')
+        #self.engine = create_engine('postgresql://'+"postgres"+':'+'pass'+'@localhost:5432/test')
 
         self.engine = create_engine("postgresql://rdk2132:password@35.211.155.104/proj1part2")
         self.conn = self.engine.connect()
@@ -53,11 +53,49 @@ class Backend:
             result_list.append(inner_list)
         return result_list 
         
-    #def get_events(self,date,team,player,league,oddsmakers):
-    #    where = ""
-    #    if date is not None:
-    #        where += " event_date = " + date + " and "
-    #    if team is not None:
-    #        where += " event_date = " + date + " and "
-              
+    def get_events(self,date,team,first_name,last_name,league):
+        if first_name is None and last_name is None:
+            where = "True "
+            if date is not None:
+                where += " AND event_date = '" + date +"' " 
+            if team is not None:
+                where += " AND (team_1 = '" + team + "' OR team_2 = '" + team + "') "
+            if league is not None:
+                where += " AND league_name = '" + league + "' "
+            query = "SELECT event_id,event_name, event_date from sporting_event where "+where+";"
+            
+            results = self.conn.execute(query)
+            result_list = []
+            for r in results:
+                result_list.append((r[0],r[1],r[2]))
+            
+            return result_list
+        else:
+            where = "True"
+            if last_name is not None:
+                where += " AND last_name = '" + last_name + "' "
+            if first_name is not None:
+                where += " AND first_name = '" + first_name + "' "  
+            if date is not None:
+                where += " AND event_date = '" + date +"' " 
+            if team is not None:
+                where += " AND (team_1 = '" + team + "' OR team_2 = '" + team + "') "
+            if league is not None:
+                where += " AND league_name = '" + league + "' "
+            query = "select event_id_a as event_id ,event_name, event_date from (select * from (select *, event_id as event_id_a from player_participation join player on player_participation.player_id = player.player_id) as merged join sporting_event on sporting_event.event_id = merged.event_id) as output_table where " +where+ ";"
+            
+            results = self.conn.execute(query)
+            result_list = []
+            for r in results:
+                result_list.append((r[0],r[1],r[2]))
+            
+            return result_list            
         
+        
+#b = Backend()
+#r = b.get_events('2022-02-04',"Mets","Jacob","DeGrom","MLB")
+#for i in r:
+#    print(i)
+    
+    
+    
